636dc83da2e982d29a5e3f5b256c5e7ac59a3296
szren001 <164858049+szren001@users.noreply.github.com>
2026-08-21 12:03:32 +0800

    Fix umdns issues and update feeds sources
    Updated script to fix umdns issues and replace feeds sources with domestic mirrors for faster downloads. Also modified libpcre dependency in Makefile.

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,48 +1,23 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# 此脚本在 feeds update 之前运行
 #
 
-function merge_package() {
-    if [[ $# -lt 3 ]]; then
-        echo "Syntax error: [$#] [$*]" >&2
-        return 1
-    fi
-    trap 'rm -rf "$tmpdir"' EXIT
-    branch="$1" curl="$2" target_dir="$3" && shift 3
-    rootdir="$PWD"
-    localdir="$target_dir"
-    [ -d "$localdir" ] || mkdir -p "$localdir"
-    tmpdir="$(mktemp -d)" || exit 1
-    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
-    cd "$tmpdir"
-    git sparse-checkout init --cone
-    git sparse-checkout set "$@"
-    for folder in "$@"; do
-        mv -f "$folder" "$rootdir/$localdir"
-    done
-    cd "$rootdir"
-}
+# 替换 feeds 源为国内镜像，加速下载
+sed -i 's_https://github.com/openwrt/packages_https://github.com.cnpmjs.org/openwrt/packages_g' feeds.conf.default
+sed -i 's_https://github.com/openwrt/luci_https://github.com.cnpmjs.org/openwrt/luci_g' feeds.conf.default
+sed -i 's_https://github.com/openwrt/routing_https://github.com.cnpmjs.org/openwrt/routing_g' feeds.conf.default
 
+# 更新 feeds
 ./scripts/feeds update -a
 
-# fix : ubus_call_umdns()=ubus.c@1385
-rm package/network/services/umdns/files/umdns.init
-rm package/network/services/umdns/files/umdns.json
-wget -P package/network/services/umdns/files https://raw.githubusercontent.com/openwrt/openwrt/main/package/network/services/umdns/files/umdns.init
-wget -P package/network/services/umdns/files https://raw.githubusercontent.com/openwrt/openwrt/main/package/network/services/umdns/files/umdns.json
+# 修复 umdns 问题
+rm -f package/network/services/umdns/files/umdns.init
+rm -f package/network/services/umdns/files/umdns.json
+wget -q -P package/network/services/umdns/files https://raw.githubusercontent.com/openwrt/openwrt/main/package/network/services/umdns/files/umdns.init
+wget -q -P package/network/services/umdns/files https://raw.githubusercontent.com/openwrt/openwrt/main/package/network/services/umdns/files/umdns.json
 
 ./scripts/feeds install -a
 
-# 修复feeds错误
-sed -i 's/+libpcre/+libpcre2/g' package/feeds/telephony/freeswitch/Makefile
-
-./scripts/feeds update -a
-./scripts/feeds install -a
+# 修复 libpcre 依赖
+sed -i 's/+libpcre/+libpcre2/g' package/feeds/telephony/freeswitch/Makefile || true

45b27f8214a18118fb6d99bd27e55460fd083ea0
szren001 <164858049+szren001@users.noreply.github.com>
2026-08-25 18:21:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,14 +13,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.8.8.8/g' package/base-files/files/bin/config_generate
 
-# 删除 Argon 主题（彻底移除）
-rm -rf feeds/luci/themes/luci-theme-argon
-
-# 修改默认主题为 Bootstrap
-sed -i 's/luci-theme-argon/luci-theme-bootstrap/g' feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-argon/luci-theme-bootstrap/g' feeds/luci/collections/luci-nginx/Makefile
-sed -i 's/luci-theme-argon/luci-theme-bootstrap/g' feeds/luci/collections/luci-ssl-nginx/Makefile
-sed -i "s/luci-static\/argon/luci-static\/bootstrap/g" feeds/luci/modules/luci-base/root/etc/config/luci 2>/dev/null || true
+# Modify default password (lizhiyang0928)
+sed -i 's/root::0:0:99999:7:::/root:$1$i5O.cpUi$oMJ0yg3MAKDXxstgShYfF::0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Modify default Hostname
 sed -i 's/ImmortalWrt/k2p/g' package/base-files/files/bin/config_generate
@@ -28,8 +22,17 @@ sed -i 's/ImmortalWrt/k2p/g' package/base-files/files/bin/config_generate
 # 最大连接数修改为65535
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 
+# 修改默认主题为 bootstrap
+sed -i 's/luci-theme-argon/luci-theme-bootstrap/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-argon/luci-theme-bootstrap/g' feeds/luci/collections/luci-nginx/Makefile
+sed -i 's/luci-theme-argon/luci-theme-bootstrap/g' feeds/luci/collections/luci-ssl-nginx/Makefile
+sed -i "s/luci-static\/argon/luci-static\/bootstrap/g" feeds/luci/modules/luci-base/root/etc/config/luci
+
 # 修改默认WiFi名称和密码 (2.4G: LZY8, 5G: LZY, 密码: lizhiyang0928)
 sed -i 's/ssid=OpenWrt/ssid=LZY8/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/ssid=OpenWrt-5G/ssid=LZY/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/key=12345678/key=lizhiyang0928/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# ========== 强制开启内核 Flow Offload 支持 ==========
+echo "CONFIG_NF_FLOW_TABLE=y" >> target/linux/ramips/mt7621/config-5.4
